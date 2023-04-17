b14570e51871ffd9d19547d3b5e76d9db29f3e58
Brian Wong <brianw7218@gmail.com>
2023-04-16 21:59:17 -0700

    Add environment variables with database connection string

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+require("dotenv").config();
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
 const app = express();
+
+const mongoDB = process.env.MONGODB_URL;
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
