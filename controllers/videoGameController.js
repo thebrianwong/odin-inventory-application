057ac3cb1b755a3e61b4810bc0ba6a12602a6c36
Brian Wong <brianw7218@gmail.com>
2023-04-18 13:37:40 -0700

    Add error handling to game controllers

--- controllers/videoGameController.js
+++ controllers/videoGameController.js
@@ -3,45 +3,70 @@ const Developer = require("../models/developer");
 const Console = require("../models/console");
 const Genre = require("../models/genre");
 
-const storeIndex = async (req, res) => {
-  const [videoGameCount, developerCount, consoleCount, genreCount] =
-    await Promise.all([
-      VideoGame.countDocuments({}).exec(),
-      Developer.countDocuments({}).exec(),
-      Console.countDocuments({}).exec(),
-      Genre.countDocuments({}).exec(),
-    ]);
-  res.render("../views/index", {
-    title: "OdinStop",
-    videoGameCount,
-    developerCount,
-    consoleCount,
-    genreCount,
-  });
+const storeIndex = async (req, res, next) => {
+  try {
+    const [videoGameCount, developerCount, consoleCount, genreCount] =
+      await Promise.all([
+        VideoGame.countDocuments({}).exec(),
+        Developer.countDocuments({}).exec(),
+        Console.countDocuments({}).exec(),
+        Genre.countDocuments({}).exec(),
+      ]);
+    res.render("../views/index", {
+      title: "OdinStop",
+      videoGameCount,
+      developerCount,
+      consoleCount,
+      genreCount,
+    });
+  } catch (err) {
+    err.status = 404;
+    next(err);
+  }
 };
 
-const displayAllGames = async (req, res) => {
-  const gameList = await VideoGame.find({})
-    .sort({ name: 1 })
-    .populate("developer")
-    .populate("genre")
-    .exec();
-  res.render("../views/videoGames/videoGamesAll", {
-    title: "OdinStop Video Games",
-    gameList,
-  });
+const displayAllGames = async (req, res, next) => {
+  try {
+    const gameList = await VideoGame.find({})
+      .sort({ name: 1 })
+      .populate("developer")
+      .populate("genre")
+      .exec();
+    if (gameList === null) {
+      const err = new Error("There was an error loading games");
+      err.status = 404;
+      next(err);
+    }
+    res.render("../views/videoGames/videoGamesAll", {
+      title: "OdinStop Video Games",
+      gameList,
+    });
+  } catch (err) {
+    err.status = 404;
+    next(err);
+  }
 };
 
-const displayOneGame = async (req, res) => {
-  const game = await VideoGame.findById(req.params.id)
-    .populate("developer")
-    .populate("console")
-    .populate("genre")
-    .exec();
-  res.render("../views/videoGames/videoGamesOne", {
-    title: req.params.id,
-    game,
-  });
+const displayOneGame = async (req, res, next) => {
+  try {
+    const game = await VideoGame.findById(req.params.id)
+      .populate("developer")
+      .populate("console")
+      .populate("genre")
+      .exec();
+    if (game === null) {
+      const err = new Error("Game does not exist");
+      err.status = 404;
+      next(err);
+    }
+    res.render("../views/videoGames/videoGamesOne", {
+      title: req.params.id,
+      game,
+    });
+  } catch (err) {
+    err.status = 404;
+    next(err);
+  }
 };
 
 module.exports = {
