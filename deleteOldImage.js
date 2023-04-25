536b7159c852ca1cbc81904a122f28e3a0e40905
Brian Wong <brianw7218@gmail.com>
2023-04-25 14:09:34 -0700

    Use path module to create image path for deletion to prevent potential conflicts between OS's

--- deleteOldImage.js
+++ deleteOldImage.js
@@ -1,7 +1,15 @@
 const fs = require("fs");
+const path = require("path");
 
 const deleteOldImage = (category, imageName) => {
-  const imagePath = `${__dirname}/public/data/uploads/${category}/${imageName}`;
+  const imagePath = path.join(
+    __dirname,
+    "public",
+    "data",
+    "uploads",
+    category,
+    imageName
+  );
   fs.unlink(imagePath, (err) => {
     if (err) {
       throw err;
